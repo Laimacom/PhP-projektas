a6ce657b48660c7e7886a51006fc28b4759c57c7
Laimacom <laima.grivaciauskaite@gmail.com>
2018-05-11 12:45:47 +0300

    Commit

---
+++ Namu darbai/php-class-07.php
@@ -0,0 +1,48 @@
+<?php
+/**
+Sukurkite klasę klasę asmuo su savybėmis: vardas, pavarde, amžius (reikšmės suteikiamos konstruktoriaus pagalba). Sukurkite klases: darbuotojas ir studentas, kurios paveldi klasę asmuo. Klasė asmuo turi metodą info, kuris išveda teksto eilutę: "Vardas, pavardė, amčius" (duomenis įstatyti iš savybių). Darbuotojas turi papildomas savybes: pareigos, atlyginimas. Studentas turti papildomą savybę: specialybė. Darbuotojo atveju metodas info turi išvesti tekstą: "Darbuotojas: vardas, pavardė, amžius, pareigos, atlyginimas" (reikšmes įstatyti iš savybių). Studento atveju metodas info turi išvesti tekstą: "Studentas: vardas, pavardė, amžius, specialybė" (reikšmes įstatyti iš savybių).
+ */
+
+class asmuo {
+    public $var;
+    public $pav;
+    public $amz;
+    public $par;
+    public $atl;
+    public $spec;
+
+    function __construct($vr, $pv, $am, $pr, $at, $sp)
+    {
+        $this->var = $vr;
+        $this->pav = $pv;
+        $this->amz = $am;
+        $this->par = $pr;
+        $this->atl = $at;
+        $this->spec = $sp;
+    }
+    function info (){
+        $s  = "%s, %s, %s";
+        echo sprintf($s, $this->var, $this->pav, $this->amz) . '<br>';
+    }
+}
+
+class darbuotojas extends asmuo {
+    function info (){
+        $s  = "Darbuotojas: %s, %s, %s, %s, %s.";
+        echo sprintf($s, $this->var, $this->pav, $this->amz, $this->par, $this->atl) . '<br>';
+    }
+}
+class studentas extends asmuo {
+    function info (){
+        $s  = "Studentas: %s, %s, %s, %s.";
+        echo sprintf($s, $this->var, $this->pav, $this->amz, $this->spec) . '<br>';
+    }
+}
+
+
+$o = new asmuo('Zigmas', 'Zigmaitis', 30, '', '', '');
+$o->info();
+$o = new darbuotojas('Jonas', 'Jonaitis', 30, 'direktorius', 2000, '');
+$o->info();
+$o = new studentas('Petras', 'Petraitis', 22, '', '', 'Ekonomika');
+$o->info();
